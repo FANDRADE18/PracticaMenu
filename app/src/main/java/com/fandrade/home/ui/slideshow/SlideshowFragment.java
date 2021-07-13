8eec282b0d39f71b889a8ea161fc9991dcf66129
FANDRADE18 <fandradetv@gmail.com>
2021-07-12 23:40:44 -0500

    Fernando Andrade Practica Menu

---
+++ app/src/main/java/com/fandrade/home/ui/slideshow/SlideshowFragment.java
@@ -0,0 +1,40 @@
+package com.fandrade.home.ui.slideshow;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.fandrade.home.R;
+import com.fandrade.home.databinding.FragmentSlideshowBinding;
+
+public class SlideshowFragment extends Fragment {
+
+    private SlideshowViewModel slideshowViewModel;
+    private FragmentSlideshowBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        slideshowViewModel =
+                new ViewModelProvider(this).get(SlideshowViewModel.class);
+
+        binding = FragmentSlideshowBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+
+        return root;
+    }
+
+
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
